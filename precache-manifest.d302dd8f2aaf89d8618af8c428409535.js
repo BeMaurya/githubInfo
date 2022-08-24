5cca5ec6f9823bf56f41969d505f30ec655e371a
Shubham Kumar <social.infoshubham@gmail.com>
2022-08-24 15:20:43 +0530

    Updates

---
+++ precache-manifest.d302dd8f2aaf89d8618af8c428409535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45975ced94060281476ac9faab472902",
+    "url": "/githubInfo/index.html"
+  },
+  {
+    "revision": "62f7b2b62213c849cfc3",
+    "url": "/githubInfo/static/css/main.392525b7.chunk.css"
+  },
+  {
+    "revision": "01e3a0c41964b68f0f07",
+    "url": "/githubInfo/static/js/2.5699120b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubInfo/static/js/2.5699120b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f7b2b62213c849cfc3",
+    "url": "/githubInfo/static/js/main.164a5a68.chunk.js"
+  },
+  {
+    "revision": "00a3547e3940bdffed60",
+    "url": "/githubInfo/static/js/runtime-main.725cc1fa.js"
+  },
+  {
+    "revision": "d617763fd5c08d9196374c29974e30ab",
+    "url": "/githubInfo/static/media/octocat.d617763f.svg"
+  }
+]);
